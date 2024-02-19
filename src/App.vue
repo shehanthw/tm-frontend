e9e25c91ff836484cad8286284adcd6048fedaf5
Shehanth <shehanthw@gmail.com>
2024-02-19 22:31:30 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- <Navbar v-if="showNavbar" /> -->
+  <div v-if="showNavbar">
+    <Navbar />
+    asdasd
+  </div>
+
+  <RouterView />
+</template>
+
+<script lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar/Navbar.vue'
+
+export default {
+  data() {
+    return {
+      showNavbar: true
+    }
+  },
+  watch: {
+    $route(to, from) {
+      this.showNavbar = !['login'].includes(to.name)
+    }
+  }
+}
+</script>
