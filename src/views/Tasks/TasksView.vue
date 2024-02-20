4215ac19ceefc738b5b7d021e7a6602bef461efe
Shehanth <shehanthw@gmail.com>
2024-02-20 21:38:18 +0530

    draggable done

--- src/views/Tasks/TasksView.vue
+++ src/views/Tasks/TasksView.vue
@@ -1,35 +1,72 @@
 <template>
   <div class="flex flex-col text-neutral-400 justify-between px-8 py-6 space-y-5">
-    <div>
+    <div class="flex space-x-4">
       <input
         class="border border-neutral-600 bg-transparent px-3 py-1 outline-none rounded-sm"
         placeholder="Add Task"
+        v-model="newTask"
       />
+      <button
+        class="px-3 py-1 hover:bg-blue-300 text-blue-900 bg-blue-400 rounded-sm text-sm"
+        @click="add"
+      >
+        Add Task
+      </button>
     </div>
 
     <!-- titles -->
     <div class="flex w-full justify-between overflow-auto custom-scrollbar">
       <div class="w-[24%] flex flex-col items-center space-y-4">
         <div class="w-[100%] bg-[#161A1D] py-3 px-4">Backlog</div>
-        <div class="w-full bg-[#161A1D] px-4 h-[5000px]">
-          <draggable class="" :list="backlog" group="tasks">
+        <div class="w-full bg-[#161A1D] px-4 min-h-[500px] py-4">
+          <!-- card -->
+          <draggable :list="backlog" group="tasks" class="flex flex-col space-y-3 min-h-[500px]">
             <template #item="{ element }">
-              <div>{{ element.name }}</div>
+              <div class="w-full px-2 py-2 bg-[#1D2125] h-[100px] cursor-pointer">
+                {{ element.name }}
+              </div>
             </template>
           </draggable>
         </div>
       </div>
       <div class="w-[24%] flex flex-col items-center space-y-4">
         <div class="w-[100%] bg-[#161A1D] py-3 px-4">In Progress</div>
-        <div class="w-full bg-[#161A1D] px-4 h-[500px]">Card</div>
+        <div class="w-full bg-[#161A1D] px-4 min-h-[500px] py-4">
+          <!-- card -->
+          <draggable :list="inProgress" group="tasks" class="flex flex-col space-y-3 min-h-[500px]">
+            <template #item="{ element }">
+              <div class="w-full px-2 py-2 bg-[#1D2125] h-[100px] cursor-pointer">
+                {{ element.name }}
+              </div>
+            </template>
+          </draggable>
+        </div>
       </div>
       <div class="w-[24%] flex flex-col items-center space-y-4">
         <div class="w-[100%] bg-[#161A1D] py-3 px-4">Ready</div>
-        <div class="w-full bg-[#161A1D] px-4 h-[500px]">Card</div>
+        <div class="w-full bg-[#161A1D] px-4 min-h-[500px] py-4">
+          <!-- card -->
+          <draggable :list="ready" group="tasks" class="flex flex-col space-y-3 min-h-[500px]">
+            <template #item="{ element }">
+              <div class="w-full px-2 py-2 bg-[#1D2125] h-[100px] cursor-pointer">
+                {{ element.name }}
+              </div>
+            </template>
+          </draggable>
+        </div>
       </div>
       <div class="w-[24%] flex flex-col items-center space-y-4">
         <div class="w-[100%] bg-[#161A1D] py-3 px-4">Done</div>
-        <div class="w-full bg-[#161A1D] px-4 h-[500px]">Card</div>
+        <div class="w-full bg-[#161A1D] px-4 min-h-[500px] py-4">
+          <!-- card -->
+          <draggable :list="done" group="tasks" class="flex flex-col space-y-3 min-h-[500px]">
+            <template #item="{ element }">
+              <div class="w-full px-2 py-2 bg-[#1D2125] h-[100px] cursor-pointer text-green-600">
+                {{ element.name }}
+              </div>
+            </template>
+          </draggable>
+        </div>
       </div>
     </div>
   </div>
